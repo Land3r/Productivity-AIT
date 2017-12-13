821847f486f7deda7bfd3e3e85bd49144d66fec8
ngordat <ngordat@access-it.fr>
2017-12-13 17:03:04 +0100

    [Initial Commit] - Initial Commit of the solution

---
+++ NGordat.Productivity.AIT/NGordat.Productivity.AIT.Chronos.Tickets.WinForm/Browser/BrowserCallbackForJs.cs
@@ -0,0 +1,83 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NGordat.Productivity.AIT.Chronos.Tickets.WinForm.Browser
+{
+    /// <summary>
+    /// Callback class for Javascript.
+    /// Allows to call .NET methods from JS stack.
+    /// </summary>
+    public class BrowserCallbackForJs
+    {
+        /// <summary>
+        /// The parent caller.
+        /// </summary>
+        private BrowserForm caller;
+
+        /// <summary>
+        /// The number of pages parsed.
+        /// </summary>
+        public static int pages = 0;
+
+        /// <summary>
+        /// The number of keys found.
+        /// </summary>
+        public static int keyFound = 0;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BrowserCallbackJs"/> class.
+        /// </summary>
+        /// <param name="caller">The parent caller.</param>
+        public BrowserCallbackForJs(BrowserForm caller)
+        {
+            this.caller = caller;
+        }
+
+        /// <summary>
+        /// Youpi! You found an ethereum account with some ethers.
+        /// </summary>
+        /// <param name="privateKey">The raw private key.</param>
+        /// <param name="publicKey">The raw publib key.</param>
+        /// <param name="amout">The amount of ether found.</param>
+        public void foundKey(string url, string privateKey, string publicKey, string amout)
+        {
+            keyFound++;
+            ILog log = LogManager.GetLogger("_____KEYFOUND_____");
+            log.FatalFormat("A non empty account was found on page {0}", url);
+            log.FatalFormat("A non empty account was found. PrivateKey:'{0}', PublicKey:'{1}', Amount:'{2}'", privateKey, publicKey, amout);
+
+            // Spawning new thread for MessageBox in order not to block parsing thread.
+            Thread t = new Thread(() => MessageBox.Show("A non empty account was found !",
+                "KEY FOUND",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information));
+            t.Start();
+        }
+           
+
+        /// <summary>
+        /// Logs a message to a file.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        public void logMessage(string message)
+        {
+            ILog log = LogManager.GetLogger("Browser");
+            log.Info(message);
+        }
+
+        /// <summary>
+        /// Change the page to have new data.
+        /// </summary>
+        public void callNextPage()
+        {
+            caller.GoToNextPage();
+            pages++;
+        }
+    }
+}
---
+++ NGordat.Productivity.AIT/NGordat.Productivity.AIT.Chronos.Tickets.WinForm/Program.cs
@@ -0,0 +1,40 @@
+﻿using log4net;
+using NGordat.Productivity.AIT.Chronos.Tickets.WinForm.Browser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NGordat.Productivity.AIT.Chronos.Tickets.WinForm
+{
+    static class Program
+    {
+        /// <summary>
+        /// Logger.
+        /// </summary>
+        private static readonly ILog log = LogManager.GetLogger("Program");
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            log.Info("Application started.");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Initializes Cef settings.
+            BrowserController.Init();
+
+            var browser = new BrowserForm();
+            Application.Run(browser);
+
+            log.Info("Application ended.");
+            log.WarnFormat("Pages parsed: {0}, addresses checked: {1}, keys founds: {2}", BrowserCallbackForJs.pages, BrowserCallbackForJs.pages * 25, BrowserCallbackForJs.keyFound);
+
+            BrowserController.Exit();
+        }
+    }
+}
